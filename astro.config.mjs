fe2859e485e974039a9a3898abe19e5222971505
mouchangrui <2020636899@qq.com>
2024-05-15 23:37:20 +0800

    add: 搭建新的工具

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import remarkToc from "remark-toc";
 import Unocss from "unocss/astro";
 import mdx from "@astrojs/mdx";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    vue(),
     mdx(),
     Unocss({
       injectReset: true,
