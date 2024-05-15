bf8a899c0980f76b21418aae1bd9349d7e0688de
mouchangrui <2020636899@qq.com>
2024-05-16 00:45:37 +0800

    fix: 调整网站结构

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from "astro/config";
 import remarkToc from "remark-toc";
 import Unocss from "unocss/astro";
 import mdx from "@astrojs/mdx";
-import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    vue(),
     mdx(),
     Unocss({
       injectReset: true,
